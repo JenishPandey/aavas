87b5578febba4f2bcef89fe0b1f94a7cc007c25d
JenishPandey <106529210+JenishPandey@users.noreply.github.com>
2024-05-24 19:44:00 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-import math
+import gzip
+from Bio import SeqIO
+
+file_path = 'gbbct100.seq.gz'
+
+snippet_length = 50
+
+with gzip.open(file_path, 'rt') as handle:
+    for record in SeqIO.parse(handle, 'genbank'):
+        seq_id = record.id
+        seq = record.seq
+        
+        print(f"ID: {seq_id}")
+        print(f"Sequence snippet: {seq[:snippet_length]}")
+
+        if len(seq) >= snippet_length:
